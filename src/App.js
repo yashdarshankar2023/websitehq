7bff141fd3932bf5866561a031d6c43debd9f07b
yashdarshankar <yashhdarshankar@gmail.com>
2023-12-03 18:06:37 +0530

    created project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+import Navbar from './pages/navbar'
+import Hero from './pages/hero'
+
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Hero/>
+
+    </div>
+  );
+}
+
+export default App;
